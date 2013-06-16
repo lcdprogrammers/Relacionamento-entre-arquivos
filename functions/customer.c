8a969670e34b6ec787ee8bd9d97b056f714a403b
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2013-06-16 15:32:42 -0300

    Menu comentado

--- functions/customer.c
+++ functions/customer.c
@@ -1,1 +1,14 @@
- 
+/*int menu()
+{
+	int option = 0;
+	
+	printf("1) Cadastrar.\n");
+	printf("2) Editar.\n");
+	printf("3) Excluir.\n");
+	printf("4) Consultar.\n");
+	printf("5) Listar todos.\n");
+	
+	scanf("%d", &option);
+	
+	return option;
+}*/
