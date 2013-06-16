68b4ad5da47ee0af2d9e47d64a0fe9ecaad0dfa5
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2013-06-16 15:32:21 -0300

    Menu principal

--- functions/app.c
+++ functions/app.c
@@ -1,5 +1,20 @@
- // Escrever na tela sempre com um espaçamento antes e depois da linha.
- void appPrintf(char *str)
- {
-	 printf("\n%s\n\n", str);
- }
+// Escrever na tela sempre com um espaçamento antes e depois da linha.
+void appPrintf(char *str)
+{
+	printf("\n%s\n\n", str);
+}
+ 
+int appMainMenu()
+{
+	int option = 0;
+	
+	printf("MENU\n=============\n\n");
+	printf("1) - Clientes.\n");
+	printf("2) - Serviços.\n");
+	printf("3) - Consultas.\n\n");
+	
+	printf("Opção: ");
+	scanf("%d", &option);
+	
+	return option;
+}
