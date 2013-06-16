cb493bd5921b9fb060a7c9218abc9be9cdce5c30
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2013-06-16 15:16:53 -0300

    Script para compilar os fontes automaticamente.

---
+++ compile.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash
+ 
+ src="main.c functions/app.c functions/customer.c functions/customer_service.c functions/service.c";
+ 
+ gcc $src -o main
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "headers/app.h"
 #include "headers/customer.h"
@@ -7,5 +8,21 @@
 
 int main()
 {
-	appPrintf("teste");
+	int option = 0;
+	
+	system("clear");
+	
+	printf("MENU\n=============\n\n");
+	printf("( 1 ) - Clientes.\n");
+	printf("( 2 ) - Serviços.\n");
+	printf("( 3 ) - Consultas.\n\n");
+	
+	printf("Opção: ");
+	scanf("%d", &option);
+	
+	switch (option) {
+		case 1: appPrintf("Clientes"); break;
+		case 2: appPrintf("Serviços"); break;
+		default: appPrintf("Consultas");
+	}
 }
