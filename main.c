8b17bb7a0bf10791dedb70b3fa7562f1b92acba6
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2013-06-16 15:01:26 -0300

    Inclusão de cabeçalhos

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-/**
- * 3 arquivos, que seriam as tabelas do sistema, uma é de clientes e suas informações, 
- * outra de serviços e os respectivos preços, e outra de consultas 
- * que faz relacionamento com os clientes e serviços. 
- */
-
 #include <stdio.h>
+
 #include "headers/app.h"
+#include "headers/customer.h"
+#include "headers/service.h"
+#include "headers/customer_service.h"
 
 int main()
 {
